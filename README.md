f177281c06061c7aca8dba1af0dc78a1423a0103
Alexander <v0xt.alex@gmail.com>
2022-01-01 20:03:10 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Solidity project template
+
+This project demonstrates an advanced Hardhat use case, integrating other tools commonly used alongside Hardhat in the ecosystem.
+
+The project comes with a custom ERC-20 token implementation with mint/burn, fees, whitelist and OpenZeppelin AccessControl. There are also tests, deploy script and CI config.
+
+### How to run
+
+Create a `.env` file using the `.env.example` template with the following content
+- [ALCHEMY_API_KEY](https://www.alchemy.com/)
+- [POLYGONSCAN_API_KEY](https://polygonscan.com/apis)
+- [CMC_API_KEY](https://coinmarketcap.com/api/)
+- [ETHERSCAN_API_KEY](https://etherscan.io/apis) - optional, polygonscan is used in config
+- MNEMONIC
+
+`.env-<network_name>` with:
+- TOKEN_NAME
+- TOKEN_SYMBOL
+- TOKEN_DECIMALS
+- TOKEN_MINT
+
+Try running some of the following tasks and don't forget to specify network (ex. `--network mumbai`):
+
+```shell
+npx hardhat coverage
+npx hardhat test test/token.test.ts
+GAS=true npx hardhat test test/token.test.ts
+npx hardhat accounts
+npx hardhat run scripts/deploy.ts --network mumbai
+npx hardhat token-balance --account <addrs> --network mumbai
+```
---
+++ hardhat.config.ts
@@ -0,0 +1,97 @@
+import "dotenv/config";
+
+import { HardhatUserConfig, NetworkUserConfig } from "hardhat/types";
+import "hardhat-docgen";
+import "hardhat-contract-sizer";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+import "./tasks/index.ts";
+
+const chainIds = {
+  mumbai: 80001,
+  rinkeby: 4,
+};
+
+let mnemonic: string;
+if (!process.env.MNEMONIC) {
+  throw new Error("Please set your MNEMONIC in a .env file");
+} else {
+  mnemonic = process.env.MNEMONIC;
+}
+
+let alchemyApiKey: string;
+if (!process.env.ALCHEMY_API_KEY) {
+  throw new Error("Please set your ALCHEMY_API_KEY in a .env file");
+} else {
+  alchemyApiKey = process.env.ALCHEMY_API_KEY;
+}
+
+function createNetworkConfig(network: keyof typeof chainIds): NetworkUserConfig {
+  const url: string = `https://polygon-${network}.g.alchemy.com/v2/${alchemyApiKey}`;
+  return {
+    accounts: {
+      count: 2,
+      mnemonic,
+    },
+    chainId: chainIds[network],
+    url,
+  };
+}
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.10",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    mumbai: createNetworkConfig("mumbai"),
+    rinkeby: createNetworkConfig("rinkeby"),
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY,
+    // Use in future hardhat-etherscan version
+    // apiKey: {
+    //   mainnet: process.env.ETHERSCAN_API_KEY,
+    //   ropsten: process.env.ETHERSCAN_API_KEY,
+    //   rinkeby: process.env.ETHERSCAN_API_KEY,
+    //   // polygon
+    //   mumbai: process.env.POLYGONSCAN_API_KEY,
+    // },
+  },
+  paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./test",
+  },
+  docgen: {
+    path: "./docs",
+    runOnCompile: true,
+  },
+  contractSizer: {
+    alphaSort: false,
+    disambiguatePaths: false,
+    runOnCompile: true,
+    strict: false,
+  },
+  gasReporter: {
+    // enabled by default
+    // enabled: process.env.GAS ? true : false,
+    currency: "USD",
+    token: "ETH",
+    gasPriceApi: "https://api.etherscan.io/api?module=proxy&action=eth_gasPrice",
+    coinmarketcap: process.env.CMC_API_KEY,
+  },
+};
+
+export default config;
