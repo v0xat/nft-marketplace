5452d29ae250432986ac5e33c53ba51328eab12e
Alexander <v0xt.alex@gmail.com>
2022-01-03 17:41:38 +0300

    refactor: erc721

---
+++ contracts/assets/erc721/Item721.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Item721 is Ownable, ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
+
+    function initMarketplace(address mp) external onlyOwner {
+        transferOwnership(mp);
+    }
+
+    function safeMint(address to, string memory tokenURI)
+        public
+        onlyOwner
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(to, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+
+    // The following functions are overrides required by Solidity
+    // if contract derives from both ERC721 & ERC721URIStorage
+
+    // function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+    //     super._burn(tokenId);
+    // }
+
+    // function tokenURI(uint256 tokenId)
+    //     public
+    //     view
+    //     override(ERC721, ERC721URIStorage)
+    //     returns (string memory)
+    // {
+    //     return super.tokenURI(tokenId);
+    // }
+}
