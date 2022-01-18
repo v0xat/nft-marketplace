95c2b519a077193cf1184ca084d026aa1e2b27ab
Alexander <v0xt.alex@gmail.com>
2022-01-18 18:51:31 +0300

    721: add AccessControl

--- contracts/assets/erc721/Academy721.sol
+++ contracts/assets/erc721/Academy721.sol
@@ -2,13 +2,16 @@
 
 pragma solidity ^0.8.10;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "../../access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 /** ERC721 item creation contract. */
-contract Academy721 is ERC721URIStorage, Ownable {
+contract Academy721 is ERC721URIStorage, AccessControl {
   using Counters for Counters.Counter;
+
+  /** Role identifier for the NFT creator role. */
+  bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
   /** A counter for tracking token ids. */
   Counters.Counter private tokenIds;
@@ -17,7 +20,9 @@ contract Academy721 is ERC721URIStorage, Ownable {
    * @param name Name of the collection.
    * @param symbol Symbol of the collection.
    */
-  constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
+  constructor(string memory name, string memory symbol) ERC721(name, symbol) {
+    _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+  }
 
   /**
    * @dev Safely mints `tokenId` and transfers it to `to`.
@@ -31,7 +36,7 @@ contract Academy721 is ERC721URIStorage, Ownable {
    */
   function safeMint(address to, string memory tokenURI)
     external
-    onlyOwner
+    onlyRole(DEFAULT_ADMIN_ROLE)
     returns (uint256)
   {
     tokenIds.increment();
@@ -45,7 +50,7 @@ contract Academy721 is ERC721URIStorage, Ownable {
 
   function burn(uint256 tokenId)
     external
-    onlyOwner
+    onlyRole(BURNER_ROLE)
   {
     _burn(tokenId);
   }
